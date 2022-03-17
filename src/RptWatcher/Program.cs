e600d249a04a3d25913fc3bdd4753054f5dc56b2
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2022-03-17 08:55:04 +0100

    Add basic project

---
+++ src/RptWatcher/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace RptWatcher;
+
+/// <summary>
+/// Holder of the entry point of the program.
+/// </summary>
+public static class Program
+{
+    /// <summary>
+    /// Entry point of the program.
+    /// </summary>
+    [STAThread]
+    public static void Main()
+    {
+        Console.WriteLine("Hello world!");
+    }
+}
